61844eebf92c713895676610755330a8691ff7da
jordanl412 <jordanlazan@gmail.com>
2022-12-16 09:47:12 -0500

    Connected database to API
    Has code to connect to local database or online database, depending which is commented out

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ let Users = Models.User;
 
 const { check, validationResult } = require('express-validator');
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+/*mongoose.connect('mongodb://localhost:27017/myFlixDB', {
+    useNewUrlParser: true, useUnifiedTopology: true
+});*/
+
+mongoose.connect('process.env.CONNECTION_URI', {
     useNewUrlParser: true, useUnifiedTopology: true
 });
 
